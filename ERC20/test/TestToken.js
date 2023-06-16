cbedb487934e521c2a00202fdf637fe63a0b0c13
Tran Van Hoai <tvhoai241223@gmail.com>
2023-06-16 11:04:23 +0700

    update

--- ERC20/test/TestToken.js
+++ ERC20/test/TestToken.js
@@ -5,13 +5,17 @@ describe("Token", function () {
     // get owner address
     const [owner, address1, address2] = await ethers.getSigners();
 
-    const lock = await ethers.deployContract("Token");
+    const Token = await ethers.getContractFactory("Token");
 
-    const ownerBalance = await lock.balanceOf(owner.address);
-    expect(await lock.balanceOf(owner.address)).to.equal(ownerBalance);
+    const hardhatToken = await Token.deploy();
 
-    // transfer 50 tokens to address1
-    await lock.connect(address1).transfer(address2.address, 50);
-    expect(await lock.balanceOf(address2.address)).to.equal(50);
+    await hardhatToken.transfer(address1.address, 50);
+    expect(await hardhatToken.balanceOf(address1.address)).to.equal(50);
+
+    // // transfer 50 tokens to address1
+    await hardhatToken.connect(address1).transfer(address2.address, 50);
+    const res = await hardhatToken.balanceOf(address2.address);
+    console.log({ res });
+    expect(await hardhatToken.balanceOf(address2.address)).to.equal(50);
   });
 });
